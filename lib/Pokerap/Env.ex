db03473aa5b7517bda8c21fc659f5c7c46727b20
LastContinue <last@continue.com>
2017-02-19 10:01:11 -0600

    Bug in environment var defaults

--- lib/Pokerap/Env.ex
+++ lib/Pokerap/Env.ex
@@ -1,23 +1,31 @@
 defmodule Pokerap.Env do
   @moduledoc """
-    Place to hold functions that expose Application env_vars.
+  Place to hold functions that expose Application env_vars.
   """
   @doc """
-  Returns value of `Application.get_env(:pokerap, :timeout, 8000)`
+  Returns value for `timeout` that is used for HTTPoison. Defaults to 8000
   """
   def timeout() do
-    Application.get_env(:pokerap, :timeout, 8000)
-    |> Integer.parse
-    |> elem(0)
+    # Application.get_env/3 only returns default when env is not defined, not nil
+    # nil can happen when Application env tries to call a System env that is not defined
+    process_env(Application.get_env(:pokerap, :recv_timeout), 8000)
   end
 
   @doc """
-  Returns value of `Application.get_env(:pokerap, :recv_timeout, 5000)`
+  Returns value for `recv_timeout` that is used for HTTPoison. Defaults to 5000
   """
   def recv_timeout() do
-    Application.get_env(:pokerap, :recv_timeout, 5000)
-    |> Integer.parse
-    |> elem(0)
+    # Application.get_env/3 only returns default when env is not defined, not nil
+    # nil can happen when Application env tries to call a System env that is not defined
+    process_env(Application.get_env(:pokerap, :recv_timeout), 5000)
+  end
+
+  defp process_env(env, default) do
+    case env do
+      nil -> default
+      value when is_bitstring(value)-> Integer.parse(value) |> elem(0)
+      value -> value
+    end
   end
 
   @doc """
@@ -25,7 +33,7 @@ defmodule Pokerap.Env do
   specified
   """
   def language() do
-    Application.get_env(:pokerap, :language, "en")
+    Application.get_env(:pokerap, :language) || "en"
   end
 
 end
